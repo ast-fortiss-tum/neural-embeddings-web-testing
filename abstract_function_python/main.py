1ebaa4d9c114cbf73359838bfd25146f11d0bf25
lgk03 <lucaakollmer@gmail.com>
2024-05-14 09:39:16 +0200

    modified SAF for using the fine-tuned model, no parametrization for choosing the model possible

--- abstract_function_python/main.py
+++ abstract_function_python/main.py
@@ -4,39 +4,17 @@ import pickle
 # new imports
 import utils
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from datasets import load_dataset
 import torch
+import pandas as pd
+from flask import Flask, request
 
-hf_path_to_model = 'lgk03/NDD-claroline_test-content'  # this should be dynamically set - currently the best performing model in terms of f1 score
-
-tokenizer = AutoTokenizer.from_pretrained(hf_path_to_model)
-model = AutoModelForSequenceClassification.from_pretrained(hf_path_to_model)
+feature = 'content'
+hf_model_name = f'lgk03/NDD-claroline_test-{feature}'  # this should be dynamically set - currently the best performing model in terms of f1 score
+tokenizer = AutoTokenizer.from_pretrained(hf_model_name)
+model = AutoModelForSequenceClassification.from_pretrained(hf_model_name)
 model.eval()  # set model into evaluation mode
 
-row = {'TODO'} # TODO one row of the dataset to be used for inference
-state1 = row['trimmed_state1']
-state2 = row['trimmed_state2']
-# map the human classification to the labels that the model will predict (1 = clone/near duplicate & 0 = distinct)
-actual_class = 1 if row['HUMAN_CLASSIFICATION'] == 1 or row['HUMAN_CLASSIFICATION'] == 0 else 0
-
-inputs = utils.preprocess_for_inference(state1, state2, tokenizer)
-predicted_class_id = utils.infer(model, inputs)
-
-is_correct = predicted_class_id == actual_class
-
-print(f"Model prediction correct? {is_correct}")
-
-print("Exiting")
-exit()
-
-
-# existing code =========
-
-import gensim
-import numpy as np
-from bs4 import BeautifulSoup
-from bs4.element import NavigableString, Comment
-from flask import Flask, request
-from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
 
@@ -57,125 +35,12 @@ def equal_route():
     obj2 = parametersJava['dom2']
 
     # compute equality of DOM objects
-    result = doc2vec_equals(obj1, obj2)
+    result = utils.bert_equals(obj1, obj2, model, tokenizer, feature)
 
-    result = "true" if result[0] == 0 else "false"
+    result = "true" if result == 1 else "false"
 
     # return true if the two objects are the clones/near-duplicates
     return result
-
-
-# abstraction function that computes similarity
-def doc2vec_equals(obj1, obj2):
-    FEATURE = None
-    if "doc2vec-distance-all" in CLASSIFIER_USED:
-        FEATURE = "doc2vec-distance-all"
-    elif "doc2vec-distance-content" in CLASSIFIER_USED:
-        FEATURE = "doc2vec-distance-content"
-    elif "doc2vec-distance-tags" in CLASSIFIER_USED:
-        FEATURE = "doc2vec-distance-tags"
-    elif "doc2vec-distance-content-tags" in CLASSIFIER_USED:
-        FEATURE = "doc2vec-distance-content-tags"
-    assert FEATURE is not None
-
-    dist = get_distance_from_embeddings(obj1, obj2, feature=FEATURE)
-    dist = dist.reshape(1, -1)
-
-    word2vec = model.predict(dist)
-    return word2vec
-
-
-def get_distance_from_embeddings(dom1, dom2, feature):
-    # 1. extract tag, content, or tags + content
-    # 2. generate line sentence? necessary?
-    # 3. create embedding
-    # 4. cosine similarity
-
-    model_tags = doc2vec_model_tags
-    model_content = doc2vec_model_content
-    model_content_tags = doc2vec_model_content_tags
-
-    corpus1 = process_html(dom1)
-    corpus2 = process_html(dom2)
-
-    emb_dom1 = None
-    emb_dom2 = None
-
-    if feature == 'doc2vec-distance-tags':
-        data1 = corpus1[1]
-        data2 = corpus2[1]
-        emb_dom1 = model_tags.infer_vector(data1).reshape(1, -1)
-        emb_dom2 = model_tags.infer_vector(data2).reshape(1, -1)
-    elif feature == 'doc2vec-distance-content':
-        data1 = corpus1[0]
-        data2 = corpus2[0]
-        emb_dom1 = model_content.infer_vector(data1).reshape(1, -1)
-        emb_dom2 = model_content.infer_vector(data2).reshape(1, -1)
-    elif feature == 'doc2vec-distance-content_tags':
-        data1 = corpus1[2]
-        data2 = corpus2[2]
-        emb_dom1 = model_content_tags.infer_vector(data1).reshape(1, -1)
-        emb_dom2 = model_content_tags.infer_vector(data2).reshape(1, -1)
-    elif feature == 'doc2vec-distance-all':
-        data1 = corpus1[1]
-        data2 = corpus2[1]
-        emb_tags1 = model_tags.infer_vector(data1).reshape(1, -1)
-        emb_tags2 = model_tags.infer_vector(data2).reshape(1, -1)
-        data1 = corpus1[0]
-        data2 = corpus2[0]
-        emb_content1 = model_content.infer_vector(data1).reshape(1, -1)
-        emb_content2 = model_content.infer_vector(data2).reshape(1, -1)
-        data1 = corpus1[2]
-        data2 = corpus2[2]
-        emb_content_tags1 = model_content_tags.infer_vector(data1).reshape(1, -1)
-        emb_content_tags2 = model_content_tags.infer_vector(data2).reshape(1, -1)
-
-        emb_dom1 = np.hstack((emb_tags1, emb_content1, emb_content_tags1))
-        emb_dom2 = np.hstack((emb_tags2, emb_content2, emb_content_tags2))
-
-    sim = cosine_similarity(emb_dom1, emb_dom2)
-    final_sim = np.array([sim[0, 0]])
-    return final_sim
-
-
-def process_html(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    corpus = ([], [], [])
-    retrieve_abstraction_from_html(soup, corpus)
-    return corpus
-
-
-def retrieve_abstraction_from_html(bs, corpus):
-    try:
-        if type(bs) == NavigableString:
-            tokens = gensim.utils.simple_preprocess(bs.string)
-            if len(tokens) > 0:
-                corpus[0].extend(tokens)
-                corpus[2].extend(tokens)
-            return
-
-        bs_has_name = bs.name != None
-        bs_is_single_tag = str(bs)[-2:] == '/>'
-
-        if bs_has_name and not bs_is_single_tag:
-            corpus[1].append(f'<{bs.name}>')
-            corpus[2].append(f'<{bs.name}>')
-        elif bs_has_name and bs_is_single_tag:
-            corpus[1].append(f'<{bs.name}/>')
-            corpus[2].append(f'<{bs.name}/>')
-        try:
-            for c in bs.children:
-                if type(c) == Comment:
-                    continue
-                retrieve_abstraction_from_html(c, corpus)
-        except Exception:
-            pass
-        if bs_has_name and not bs_is_single_tag:
-            corpus[1].append(f'</{bs.name}>')
-            corpus[2].append(f'</{bs.name}>')
-    except Exception as e:
-        print('html structure content error', e)
-        pass
 
 
 if __name__ == "__main__":
--- abstract_function_python/utils.py
+++ abstract_function_python/utils.py
@@ -1,18 +1,52 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
+from bs4 import BeautifulSoup, Comment
+from bs4.element import NavigableString
+import gensim
 
 # File to define utils for abstract function python
 
+def process_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    corpus = ([], [], [])
+    retrieve_abstraction_from_html(soup, corpus)
+    return corpus
+
+
+def retrieve_abstraction_from_html(bs, corpus):
+    try:
+        if type(bs) == NavigableString:
+            tokens = gensim.utils.simple_preprocess(bs.string)
+            if len(tokens) > 0:
+                corpus[0].extend(tokens)
+                corpus[2].extend(tokens)
+            return
+
+        bs_has_name = bs.name != None
+        bs_is_single_tag = str(bs)[-2:] == '/>'
+
+        if bs_has_name and not bs_is_single_tag:
+            corpus[1].append(f'<{bs.name}>')
+            corpus[2].append(f'<{bs.name}>')
+        elif bs_has_name and bs_is_single_tag:
+            corpus[1].append(f'<{bs.name}/>')
+            corpus[2].append(f'<{bs.name}/>')
+        try:
+            for c in bs.children:
+                if type(c) == Comment:
+                    continue
+                retrieve_abstraction_from_html(c, corpus)
+        except Exception:
+            pass
+        if bs_has_name and not bs_is_single_tag:
+            corpus[1].append(f'</{bs.name}>')
+            corpus[2].append(f'</{bs.name}>')
+    except Exception as e:
+        print('html structure content error', e)
+        pass
+
+# Preprocesses the input states for inference by tokenizing them using the provided tokenizer.
 def preprocess_for_inference(state1, state2, tokenizer):
-    """
-    Preprocesses the input states for inference by tokenizing them using the provided tokenizer.
-    :param state1: respective html representation of the first state (i.e. state12.html.content_tags)
-    :param state2: respective html representation of the second state (i.e. state12.html.content_tags)
-    :param tokenizer: The tokenizer to use for tokenization.
-    :return: A dictionary containing the tokenized representations of `state1` and `state2`.
-      This dictionary includes the following keys: 'input_ids', 'token_type_ids', 'attention_mask',
-      where each key maps to a tensor representing the respective tokenized data.
-    """
     tokenized_inputs = tokenizer(state1, state2,
                                  padding='max_length',
                                  truncation='longest_first',
@@ -20,14 +54,8 @@ def preprocess_for_inference(state1, state2, tokenizer):
                                  return_tensors='pt')
     return tokenized_inputs
 
-
-def infer(model, inputs):
-    """
-    Runs inference on the provided inputs using the provided model.
-    :param model: The model to use for inference.
-    :param inputs: The inputs to run inference on.
-    :return: The predicted class id.
-    """
+# Runs inference on the provided inputs using the provided model.
+def get_prediction(model, inputs):
     with torch.no_grad():  # disable gradient computation
         outputs = model(**inputs)
     # extract logits and apply softmax
@@ -35,3 +63,28 @@ def infer(model, inputs):
     # predict the class with the highest probability
     predicted_class_id = probabilities.argmax(dim=-1).item()
     return predicted_class_id
+
+def bert_equals(dom1, dom2, model, tokenizer, feature='content_tags'):
+    """
+    1. Extract tag, content, or tags + content
+    2. Tokenize the extracted data
+    3. Predict the similarity between the two states using a fine-tuned BERT model
+    :return: The predicted class (0 for distinct, 1 for clone/near-duplicate).
+    """
+    corpus1 = process_html(dom1)
+    corpus2 = process_html(dom2)
+    if feature.endswith('content_tags'):
+        data1 = corpus1[2]
+        data2 = corpus2[2]
+    elif feature.endswith('tags'):
+        data1 = corpus1[1]
+        data2 = corpus2[1]
+    elif feature.endswith('content'):
+        data1 = corpus1[0]
+        data2 = corpus2[0]
+    else:
+        raise ValueError(f'Invalid feature type: {feature}') #TODO: handling of distance-all
+
+    processed_inputs = preprocess_for_inference(data1, data2, tokenizer)
+    predicted = get_prediction(model, processed_inputs)
+    return predicted
